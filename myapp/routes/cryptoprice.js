04055ee57086bcd067937e4d679029da516f0620
Vihang Kale <59720809+vihangkale@users.noreply.github.com>
2020-09-21 13:31:30 +0530

    Add files via upload

---
+++ myapp/routes/cryptoprice.js
@@ -0,0 +1,17 @@
+//var express = require('express');
+//var app = express()
+
+
+
+//app.post('/',function(req,res) {
+//	res.render('cryptoprice', {cryptocurrencyId:"bitcoin", email:"dev@chainworks.io"})
+//	res.send("I am a post method");
+
+})
+
+//app.listen(3000,sfunction() {
+//	console.log("started on port:3000")
+//});
+
+
+
